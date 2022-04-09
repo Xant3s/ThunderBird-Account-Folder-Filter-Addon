ebec48bc4ca1e75e545cffc0048e266bd042a16e
Samuel Truman <mail@samueltruman.com>
2022-04-09 14:23:29 +0200

    Don't show Local Folders, properly clone folder pane

--- implementation.js
+++ implementation.js
@@ -23,16 +23,17 @@ var myapi = class extends ExtensionCommon.ExtensionAPI {
                         return false;
 
                     function manipulate() {
-                        const originalRowMap = this.window.gFolderTreeView._rowMap
+                        const originalRowMap = this.window.gFolderTreeView._rowMap.slice()
 
                         // Original
-                        let localFolderID = 0
+                        // let localFolderID = 0
                         // for(let i = this.window.gFolderTreeView._rowMap.length -1; i >= 0 ; i--){
                         //     if(this.window.gFolderTreeView._rowMap[i]._folder?.hostname == 'Local Folders'){
                         //         localFolderID = i
-                                // this.window.gFolderTreeView._rowMap.splice(i, 1);
+                        //         this.window.gFolderTreeView._rowMap.splice(i, 1);
                         //     }
                         // }
+                        // this.window.gFolderTreeView._rowMap = originalRowMap.slice()
                         // this.window.gFolderTreeView._rowMap[0] = this.window.gFolderTreeView._rowMap[localFolderID]
                         ////! this.window.gFolderTreeView._rowMap[0]._folder.server.prettyName = 'Local Folders'
 
@@ -57,20 +58,29 @@ var myapi = class extends ExtensionCommon.ExtensionAPI {
                         // TODO: add another window above the folder pane to DOM, where user can select account.
                         // e.g. modify folder pane toolbar or replicate folder pane toolbar
                         for(let accountName of accountNames) {
+                            if(accountName === 'Local Folders') {
+                                continue
+                            }
                             let span = this.window.document.createElement('span')
                             let accountBtn = this.window.document.createElement('button')
                             // accountBtn.setAttribute('style', 'flex-wrap: wrap;')
                             accountBtn.addEventListener('click', () => {
-                                console.log(`${accountName} clicked`)
+                                // Restore all
+                                // TODO: fix
+                                // this.window.gFolderTreeView._rowMap = originalRowMap.slice()
+                                // // Remove all but one account
+                                // for(let i = this.window.gFolderTreeView._rowMap.length -1; i >= 0 ; i--){
+                                //     console.log(this.window.gFolderTreeView._rowMap[i]._folder?.prettyName)
+                                //     if(this.window.gFolderTreeView._rowMap[i]._folder?.prettyName !== accountName){
+                                //         this.window.gFolderTreeView._rowMap.splice(i, 1);
+                                //     }
+                                // }
                             })
                             folderPanelHeader.children[1].appendChild(accountBtn)
                             let toolbarBtnText = this.window.document.createElement('label')
-                            console.log(accountName)
                             let localFolderIndex = accounts.findIndex(account => account.name === accountName)
                             let unread = accounts[localFolderIndex]?.unreadMessagesTotal
-                            console.log(unread)
                             toolbarBtnText.innerHTML = `${accountName} (${unread})`
-                            // toolbarBtnText.innerHTML = `${accountName}`
                             toolbarBtnText.classList.add('toolbarbutton-text')
                             accountBtn.appendChild(toolbarBtnText)
                             span.appendChild(accountBtn)
