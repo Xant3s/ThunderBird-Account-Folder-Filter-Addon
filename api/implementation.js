2e820b9e6dd21d7155db760f59bb9b3625f16983
Samuel Truman <mail@samueltruman.com>
2022-04-29 23:30:42 +0200

    Updated code styling

--- api/implementation.js
+++ api/implementation.js
@@ -1,56 +1,54 @@
-var { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
-var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
-var { MailUtils } = ChromeUtils.import("resource:///modules/MailUtils.jsm");
-var { MailServices } = ChromeUtils.import("resource:///modules/MailServices.jsm");
+var {ExtensionCommon} = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm")
+var {Services} = ChromeUtils.import("resource://gre/modules/Services.jsm")
+var {MailUtils} = ChromeUtils.import("resource:///modules/MailUtils.jsm")
+var {MailServices} = ChromeUtils.import("resource:///modules/MailServices.jsm")
 
 
 var AccountsFolderFilter = class extends ExtensionCommon.ExtensionAPI {
     getAPI(context) {
-        let self = this;
-        context.callOnClose(this);
+        let self = this
+        context.callOnClose(this)
 
         // keep track of windows manipulated by this API
-        this.manipulatedWindows = [];
+        this.manipulatedWindows = []
 
         return {
             AccountsFolderFilter: {
                 async showOnly(windowId, enforceRebuild, accounts, accountName) {
-                    if(!windowId)
-                        return false;
+                    if(!windowId) return false
 
                     // get the real window belonging to the WebExtension window ID
-                    let requestedWindow = context.extension.windowManager.get(windowId, context).window;
-                    if(!requestedWindow)
-                        return false;
+                    let requestedWindow = context.extension.windowManager.get(windowId, context).window
+                    if(!requestedWindow) return false
 
                     function foo() {
-                        for(let i = this.window.gFolderTreeView._rowMap.length -1; i >= 0 ; i--){
-                            if(this.window.gFolderTreeView._rowMap[i]._folder?.prettyName !== accountName){
-                                this.window.gFolderTreeView._rowMap.splice(i, 1);
+                        for(let i = this.window.gFolderTreeView._rowMap.length - 1; i >= 0; i--) {
+                            if(this.window.gFolderTreeView._rowMap[i]._folder?.prettyName !== accountName) {
+                                this.window.gFolderTreeView._rowMap.splice(i, 1)
                             }
                         }
                     }
 
-                    let callback = foo.bind(requestedWindow);
-                    requestedWindow.addEventListener("mapRebuild", callback);
-                    self.manipulatedWindows.push({requestedWindow, callback});
+                    let callback = foo.bind(requestedWindow)
+                    requestedWindow.addEventListener("mapRebuild", callback)
+                    self.manipulatedWindows.push({requestedWindow, callback})
 
                     if(enforceRebuild) {
-                        requestedWindow.gFolderTreeView._rebuild();
+                        requestedWindow.gFolderTreeView._rebuild()
                     }
                 },
                 async showAll() {
                     for(let manipulated of self.manipulatedWindows) {
-                        manipulated.requestedWindow.removeEventListener("mapRebuild", manipulated.callback);
-                        manipulated.requestedWindow.gFolderTreeView._rebuild();
+                        manipulated.requestedWindow.removeEventListener("mapRebuild", manipulated.callback)
+                        manipulated.requestedWindow.gFolderTreeView._rebuild()
                     }
                 },
                 async addAccountButtons(windowId, enforceRebuild, accounts) {
-                    if(!windowId) return false;
+                    if(!windowId) return false
 
                     //get the real window belonging to the WebExtension window ID
-                    let requestedWindow = context.extension.windowManager.get(windowId, context).window;
-                    if(!requestedWindow) return false;
+                    let requestedWindow = context.extension.windowManager.get(windowId, context).window
+                    if(!requestedWindow) return false
 
                     let accountNames = accounts.map(account => account.name)
 
@@ -65,7 +63,7 @@ var AccountsFolderFilter = class extends ExtensionCommon.ExtensionAPI {
                         folderPanelHeader.insertBefore(breakLine, buttonContainer)
 
                         for(let accountName of accountNames) {
-                            if (accountName === 'Local Folders') continue
+                            if(accountName === 'Local Folders') continue
 
                             async function bar() {
                                 await api.showAll()
@@ -87,6 +85,7 @@ var AccountsFolderFilter = class extends ExtensionCommon.ExtensionAPI {
                         async function foobar() {
                             await api.showAll()
                         }
+
                         let showAllBtn = this.window.document.createElement('button')
                         showAllBtn.innerText = 'Show all'
                         showAllBtn.style.display = 'block'
@@ -95,15 +94,15 @@ var AccountsFolderFilter = class extends ExtensionCommon.ExtensionAPI {
 
                     }
 
-                    let callback = manipulate.bind(requestedWindow, this);
+                    let callback = manipulate.bind(requestedWindow, this)
                     callback(this)
                 },
                 async updateUnreadCounts(windowId, enforceRebuild, accounts) {
-                    if(!windowId) return false;
+                    if(!windowId) return false
 
                     //get the real window belonging to the WebExtension window ID
-                    let requestedWindow = context.extension.windowManager.get(windowId, context).window;
-                    if(!requestedWindow) return false;
+                    let requestedWindow = context.extension.windowManager.get(windowId, context).window
+                    if(!requestedWindow) return false
 
                     let accountNames = accounts.map(account => account.name)
 
@@ -118,36 +117,36 @@ var AccountsFolderFilter = class extends ExtensionCommon.ExtensionAPI {
                         }
                     }
 
-                    let callback = manipulate.bind(requestedWindow);
+                    let callback = manipulate.bind(requestedWindow)
                     callback(this)
                 },
                 async selectInboxOfAccount() {
-                    let recentWindow = Services.wm.getMostRecentWindow("mail:3pane");
+                    let recentWindow = Services.wm.getMostRecentWindow("mail:3pane")
                     // Select the account. This is useful if the account is collapsed and no inbox is selectable.
                     recentWindow.gFolderTreeView.selection.select(0)
                     // Select the inbox. Will have no effect if the account is collapsed.
                     recentWindow.gFolderTreeView.selection.select(1)
                 }
             }
-        };
+        }
     }
 
     close() {
         // This is called when the API shuts down. This API could be invoked multiple times in different contexts
         // and we therefore need to cleanup actions done by this API here.
         for(let manipulated of this.manipulatedWindows) {
-            manipulated.requestedWindow.removeEventListener("mapRebuild", manipulated.callback);
+            manipulated.requestedWindow.removeEventListener("mapRebuild", manipulated.callback)
             manipulated.requestedWindow.document.getElementById('accountButtonsContainer').remove()
             manipulated.requestedWindow.document.getElementById('accountButtonsBreakLine').remove()
-            manipulated.requestedWindow.gFolderTreeView._rebuild();
+            manipulated.requestedWindow.gFolderTreeView._rebuild()
         }
     }
 
     onShutdown(isAppShutdown) {
         // This is called when the add-on or Thunderbird itself is shutting down.
-        if (isAppShutdown) {
-            return;
+        if(isAppShutdown) {
+            return
         }
-        Services.obs.notifyObservers(null, "startupcache-invalidate", null);
+        Services.obs.notifyObservers(null, "startupcache-invalidate", null)
     }
-};
+}
